bf76a7585eb96009e2820b65327af9afbe4ca95b
StackDoubleFlow <ojaslandge@gmail.com>
2021-12-18 14:42:20 -0600

    Show constant output in dot file output

--- src/dot_render.rs
+++ src/dot_render.rs
@@ -3,16 +3,21 @@ use std::fs::OpenOptions;
 use std::io::Write;
 use std::path::Path;
 
-use crate::node::{LinkType, Node};
+use crate::node::{LinkType, Node, NodeType};
 
 pub fn render(nodes: &[Node], path: impl AsRef<Path>) -> Result<()> {
     let mut f = OpenOptions::new().create(true).write(true).open(path)?;
     write!(f, "digraph{{")?;
     for (i, node) in nodes.iter().enumerate() {
+        let name = if matches!(node.ty, NodeType::Constant) {
+            format!("Constant({})", node.output_power)
+        } else {
+            format!("{:?}", node.ty)
+        };
         write!(
             f,
-            "n{}[label=\"{}: {:?}\\n({}, {}, {})\"];",
-            i, i, node.ty, node.pos.x, node.pos.y, node.pos.z
+            "n{}[label=\"{}: {}\\n({}, {}, {})\"];",
+            i, i, name, node.pos.x, node.pos.y, node.pos.z
         )?;
         for link in &node.inputs {
             let color = match link.ty {
